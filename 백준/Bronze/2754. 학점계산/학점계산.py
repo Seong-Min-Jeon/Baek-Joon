8c094b771e1572e632b3d20f946b084328ec87bd
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2024-11-02 22:26:26 +0900

    [Bronze III] Title: 학점계산, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,16 @@
+a=input()
+s=0
+if(a=='A+'): s=4.3
+elif(a=='A0'): s=4.0
+elif(a=='A-'): s=3.7
+elif(a=='B+'): s=3.3
+elif(a=='B0'): s=3.0
+elif(a=='B-'): s=2.7
+elif(a=='C+'): s=2.3
+elif(a=='C0'): s=2.0
+elif(a=='C-'): s=1.7
+elif(a=='D+'): s=1.3
+elif(a=='D0'): s=1.0
+elif(a=='D-'): s=0.7
+else: s=0.0
+print(s)
