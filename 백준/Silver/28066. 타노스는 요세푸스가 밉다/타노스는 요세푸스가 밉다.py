41ef2e462ce43b032f4a1c7d2723d823659be105
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-01-06 01:14:35 +0900

    [Silver II] Title: 타노스는 요세푸스가 밉다, Time: 648 ms, Memory: 80688 KB -BaekjoonHub

--- 백준/Silver/28066. 타노스는 요세푸스가 밉다/타노스는 요세푸스가 밉다.py
+++ 백준/Silver/28066. 타노스는 요세푸스가 밉다/타노스는 요세푸스가 밉다.py
@@ -3,7 +3,6 @@ n,k=map(int,input().split())
 d=deque([i for i in range(1,n+1)])
 while len(d)>1:
     t=d[0]
-    for _ in range(k):
-        if(len(d)>0): d.popleft()
+    for _ in range(len(d) if k>len(d) else k): d.popleft()
     d.append(t)
 print(d[0])
