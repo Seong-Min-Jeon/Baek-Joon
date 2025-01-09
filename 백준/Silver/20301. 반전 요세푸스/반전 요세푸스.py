b082d919b82463d4d72179859d766fe95fc9c9c5
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-01-10 00:41:00 +0900

    [Silver III] Title: 반전 요세푸스, Time: 384 ms, Memory: 249656 KB -BaekjoonHub

--- 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
@@ -1,9 +1,14 @@
 from collections import deque
 n,k,m=map(int,input().split())
 d=deque([i for i in range(1,n+1)])
-c,t=0,-1
+c,t=0,1
 for i in range(1,n+1):
-    d.rotate(k*t)
-    if(t==-1): print(d.pop())
-    else: print(d.popleft())
+    if(t==1): 
+        for _ in range(k):
+            d.append(d.popleft())
+        print(d.pop())
+    else:
+        for _ in range(k):
+            d.appendleft(d.pop())
+        print(d.popleft())
     if(i%m==0): t*=-1
