5e5ac29c89947fe9c47e089d3ea97e7feb1f3442
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-01-13 02:02:57 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.py
@@ -1,8 +1,8 @@
 a=int(input())
-if(a==0): print('NO'); exit()
 v=2432902008176640000
 for i in range(20,0,-1):
     v//=i
-    if(a>=v): a-=v
-if(a==0): print('YES')
-else: print('NO')
+    if(a>=v): 
+        a-=v
+        if(a==0): print('YES'); exit()
+print('NO')
