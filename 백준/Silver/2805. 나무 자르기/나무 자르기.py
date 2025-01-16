bc065aa515fed05905fc048dfe0e9497e4d4e3ed
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-01-17 00:14:07 +0900

    [Silver II] Title: 나무 자르기, Time: 2196 ms, Memory: 145580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,8 +1,9 @@
 n,m=map(int,input().split())
 l=list(map(int,input().split()))
-x,y,z=0,max(l)//2,max(l)
-while True:
-    t = sum(e-y for e in l if y<e)
-    if(t<m): y,z=(x+y)//2,y
-    else: x,y=y,(y+z)//2
-    if(x==y or y==z): print(y); break
+x,z=0,max(l)
+while x<=z:
+    y=(x+z)//2
+    t=sum(e-y for e in l if y<e)
+    if(t<m): z=y-1
+    else: x=y+1
+print(z)
