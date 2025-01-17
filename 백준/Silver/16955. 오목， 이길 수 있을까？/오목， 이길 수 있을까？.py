2ba35a8101f7cf2b9d9bd41e0949b916b00882a7
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-01-18 01:06:47 +0900

    [Silver IV] Title: 오목, 이길 수 있을까?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
+++ 백준/Silver/16955. 오목， 이길 수 있을까？/오목， 이길 수 있을까？.py
@@ -1,7 +1,7 @@
 def f():
     for i in range(10):
         for j in range(10):
-            if(l[i][j]=='X'):                        
+            if(l[i][j]=='X'):
                 if(j<6 and len(set([l[i][j],l[i][j+1],l[i][j+2],l[i][j+3],l[i][j+4]]))==1): return 1
                 elif(i<6 and len(set([l[i][j],l[i+1][j],l[i+2][j],l[i+3][j],l[i+4][j]]))==1): return 1
                 elif(i<6 and j<6 and len(set([l[i][j],l[i+1][j+1],l[i+2][j+2],l[i+3][j+3],l[i+4][j+4]]))==1): return 1
