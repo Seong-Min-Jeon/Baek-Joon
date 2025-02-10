dfcfdc5327986a60dd0129c82d04c92040082274
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-02-10 15:18:52 +0900

    [Silver I] Title: 다이아몬드, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10571. 다이아몬드/다이아몬드.py
+++ 백준/Silver/10571. 다이아몬드/다이아몬드.py
@@ -2,10 +2,9 @@ import sys
 I=sys.stdin.readline
 for _ in range(int(I())):
     n=int(I())
-    l=[tuple(map(float,I().split())) for _ in range(n)]
-    m=[0 for _ in range(n)]
-    for i in range(n):
-        m[i]=1
+    l=[list(map(float,I().split())) for _ in range(n)]
+    m=[1 for _ in range(n)]
+    for i in range(n):        
         for j in range(i):
             if(l[i][0]>l[j][0] and l[i][1]<l[j][1]):
                 m[i]=max(m[i],m[j]+1)
