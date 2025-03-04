f0366ad471f2335e4c058c541f656bb2c5f04cd9
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-03-04 13:42:38 +0900

    [Gold V] Title: 트리와 쿼리, Time: 372 ms, Memory: 68600 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.py
@@ -1,26 +1,19 @@
 import sys
 sys.setrecursionlimit(10**6)
 I=sys.stdin.readline
-
 def d(s):
     for e in l[s]:
-        if(v[e]==[]):
-            v[s].append(e)
-            d(e)
-
-def f(s):
-    for e in v[s]: c[s]+=f(e)
-    return c[s]
-
+        if(v[e]==1):
+            l[e].remove(s)
+            v[s]+=d(e)
+    return v[s]
 n,r,q=map(int,I().split())
 l=[[] for _ in range(n+1)]
 for _ in range(n-1):
     x,y=map(int,I().split())
     l[x].append(y)
     l[y].append(x)
-v=[[] for _ in range(n+1)]
+v=[1 for _ in range(n+1)]
 d(r)
-c=[1 for _ in range(n+1)]
-f(r)
 for _ in range(q):
-    print(c[int(I())])
+    print(v[int(I())])
