d27b4f0ffa437210e46e0676869fa0763a67cba0
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-03-08 20:41:54 +0900

    [Gold V] Title: 시철이가 사랑한 GCD, Time: 336 ms, Memory: 55416 KB -BaekjoonHub

--- 백준/Gold/21870. 시철이가 사랑한 GCD/시철이가 사랑한 GCD.py
+++ 백준/Gold/21870. 시철이가 사랑한 GCD/시철이가 사랑한 GCD.py
@@ -1,13 +1,8 @@
 import math
 def f(l,s):
-    if(len(l)==1):        
-        r.append(s+l[0])
-        return
+    if(len(l)==1): return s+l[0]
     a=l[:len(l)//2]
     b=l[len(l)//2:]
-    f(b,s+math.gcd(*a))
-    f(a,s+math.gcd(*b))
-n=input()
-r=[]
-f(list(map(int,input().split())),0)
-print(max(r))
+    return max(f(b,s+math.gcd(*a)),f(a,s+math.gcd(*b)))
+input()
+print(f(list(map(int,input().split())),0))
