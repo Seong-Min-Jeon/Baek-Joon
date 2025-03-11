0242b736f7cb06436dc69927d358119bbfff42a8
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-03-11 13:51:17 +0900

    [Platinum V] Title: 개코전쟁, Time: 1484 ms, Memory: 88600 KB -BaekjoonHub

---
+++ 백준/Platinum/2325. 개코전쟁/개코전쟁.py
@@ -0,0 +1,37 @@
+import heapq
+import sys
+
+def f(l,f,a,b):
+    r=[1e9]*(n+1)
+    r[1]=0
+    p=[0]*(n+1)
+    q=[(0,1)]
+    while q:
+        d,x=heapq.heappop(q)
+        if(r[x]<d): continue
+        for y,z in l[x]:
+            if((x==a and y==b) or (x==b and y==a)): continue
+            if(r[y]>d+z):
+                heapq.heappush(q,(d+z,y))
+                r[y]=d+z
+                p[y]=x
+    if(f): return r[-1]
+    else: return p
+
+I=sys.stdin.readline
+n,m=map(int,I().split())
+l=[[] for _ in range(n+1)]
+for _ in range(m):
+    x,y,z=map(int,I().split())
+    l[x].append((y,z))
+    l[y].append((x,z))
+p=f(l,0,0,0)
+t=[]
+x=n
+while x!=1:
+    t.append((x,p[x]))
+    x=p[x]
+r=0
+for a,b in t:
+    r=max(r,f(l,1,a,b))
+print(r)
