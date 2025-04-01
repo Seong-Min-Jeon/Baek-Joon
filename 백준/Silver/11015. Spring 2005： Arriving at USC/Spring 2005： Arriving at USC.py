57893a38225d55dbd9809d75f01c26083648315f
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-04-01 21:35:11 +0900

    [Silver V] Title: Spring 2005: Arriving at USC, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11015. Spring 2005： Arriving at USC/Spring 2005： Arriving at USC.py
+++ 백준/Silver/11015. Spring 2005： Arriving at USC/Spring 2005： Arriving at USC.py
@@ -1,16 +1,13 @@
 for k in range(int(input())):
+    print(f'Data Set {k+1}:')
     l=[input() for _ in range(int(input()))]
     s=input().strip()
-    r=[]
     for n in l:
         c=0
         for i in n:
             if(i.lower()==s[c].lower()):
                 c+=1
             if(c==len(s)):
-                r.append(n)
+                print(n)
                 break
-    print(f'Data Set {k+1}:')
-    for e in r:
-        print(e)
     print()
