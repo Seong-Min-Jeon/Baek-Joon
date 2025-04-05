9dc32319c473df8212f367ca4f4c9e6b56d304d6
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-04-05 14:57:47 +0900

    [Silver III] Title: 세 번 이내에 사과를 먹자, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
+++ 백준/Silver/26169. 세 번 이내에 사과를 먹자/세 번 이내에 사과를 먹자.py
@@ -1,13 +1,14 @@
-import copy
+def dfs(x,y,c,d):
+    if(c>1): print(1); exit()
+    if(d==3): return    
+    for i,j in ((1,0),(-1,0),(0,1),(0,-1)):
+        if(0<=i+x<5 and 0<=j+y<5 and l[x+i][y+j]!=-1 and v[i+x][j+y]==0):
+            v[i+x][j+y]=1
+            dfs(i+x,j+y,c+l[x+i][y+j],d+1)
+            v[i+x][j+y]=0
 l=[list(map(int,input().split())) for _ in range(5)]
 x,y=map(int,input().split())
-q=[(x,y,0,0,copy.deepcopy(l))]
-while q:
-    x,y,c,d,t=q.pop(0)
-    if(c>=2): print(1); exit()
-    t[x][y]=-1
-    for i,j in ((1,0),(-1,0),(0,1),(0,-1)):
-        if(0<=i+x<5 and 0<=j+y<5 and t[x+i][y+j]!=-1):
-            if(t[x+i][y+j]==1): q.append((x+i,y+j,c+1,d+1,copy.deepcopy(t)))
-            elif(d<2): q.append((x+i,y+j,c,d+1,copy.deepcopy(t)))
+v=[[0]*5 for _ in range(5)]
+v[x][y]=1
+dfs(x,y,0,0)
 print(0)
