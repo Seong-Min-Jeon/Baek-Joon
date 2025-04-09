98e499bf23b852b918dd685cc3a301fad1941f8b
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-04-09 21:20:00 +0900

    [Silver II] Title: 숫자판 점프, Time: 48 ms, Memory: 33948 KB -BaekjoonHub

--- 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -1,13 +1,8 @@
-l=[list(map(int,input().split())) for _ in range(5)]
+def f(i,j,t):
+    if(len(t)==6): s.add(t); return
+    for x,y in ((1,0),(-1,0),(0,1),(0,-1)):
+        if(0<=x+i<5 and 0<=y+j<5): f(x+i,y+j,t+l[x+i][y+j])
+l=[input().split() for _ in range(5)]
 s=set()
-for i in range(5):
-    for j in range(5):
-        q=[(i,j,'')]
-        while q:
-            i,j,k=q.pop(0)
-            for x,y in ((1,0),(-1,0),(0,1),(0,-1)):
-                if(0<=x+i<5 and 0<=y+j<5):
-                    t=k+str(l[x+i][y+j])
-                    if(len(t)==6): s.add(t)
-                    else: q.append((x+i,y+j,t))
+for i in range(25): f(i//5,i%5,'')
 print(len(s))
