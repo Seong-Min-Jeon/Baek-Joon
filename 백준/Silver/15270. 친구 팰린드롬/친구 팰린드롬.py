c515aa65bc560600a0a8a27a79160148c9e563ba
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-04-10 20:03:03 +0900

    [Silver I] Title: 친구 팰린드롬, Time: 1432 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15270. 친구 팰린드롬/친구 팰린드롬.py
+++ 백준/Silver/15270. 친구 팰린드롬/친구 팰린드롬.py
@@ -1,4 +1,3 @@
-import copy
 n,m=map(int,input().split())
 l=[[] for _ in range(n+1)]
 for _ in range(m):
@@ -6,26 +5,15 @@ for _ in range(m):
     l[a].append(b)
     l[b].append(a)
 m=0
-def f(l,c):
+def f(s,i,c):
     global m
-    b=0
-    for p in l:
-        b+=len(p)
-    if(b==0): m=max(m,c); return
-    a,i=0,0
-    for j,p in enumerate(l):
-        if(len(p)>a): a=len(p); i=j
-    g=0
+    if(i==n): m=max(m,c); return
     for p in l[i]:
-        if(len(l[p])==0): g+=len(l[p]); continue
-        z=copy.deepcopy(l)
-        z[i].clear()
-        z[p].clear()
-        f(z,c+2)
-    if(g==0):
-        z=copy.deepcopy(l)
-        z[i].clear()
-        f(z,c)
-f(l,0)
+        if(i in s or p in s): continue
+        s.add(i); s.add(p)
+        f(s,i+1,c+2)
+        s.remove(i); s.remove(p)
+    f(s,i+1,c)
+f(set(),0,0)
 if(m<n): m+=1
 print(m)
