a4443048ee0b1327d520ff2f8d46082f6b3c558c
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-04-11 20:41:23 +0900

    [Silver I] Title: 컴백홈, Time: 120 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.py
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -1,19 +1,22 @@
+def f(x,y,d,c,k):
+    global m
+    if(d==k):
+        if(x==0 and y==c-1): m+=1
+        return
+    for i,j in ((1,0),(-1,0),(0,1),(0,-1)):
+        if(0<=x+i<r and 0<=y+j<c and v[x+i][y+j]==0 and l[x+i][y+j]!=1):
+            v[x+i][y+j]=1
+            f(x+i,y+j,d+1,c,k)
+            v[x+i][y+j]=0
+
 r,c,k=map(int,input().split())
 l=[[0]*c for _ in range(r)]
 for i in range(r):
     for j,p in enumerate(list(input().strip())):
         if(p=='.'): l[i][j]=0
         else: l[i][j]=1
-q=[(r-1,0,1,[(r-1,0)])]
-m=[]
-while q:
-    x,y,d,s=q.pop(0)
-    for i,j in ((1,0),(-1,0),(0,1),(0,-1)):
-        t=s.copy()
-        if(0<=x+i<r and 0<=y+j<c and (x+i,y+j) not in t and l[x+i][y+j]!=1):
-            t.append((x+i,y+j))
-            if(d+1==k and x+i==0 and y+j==c-1):
-                if(t not in m): m.append(t)
-                continue
-            q.append((x+i,y+j,d+1,t))
-print(len(m))
+v=[[0]*c for _ in range(r)]
+v[r-1][0]=1
+m=0
+f(r-1,0,1,c,k)
+print(m)
