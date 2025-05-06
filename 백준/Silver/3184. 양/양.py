625e1ea3e9b752868578ee082b29bff46481ae25
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-05-06 13:18:37 +0900

    [Silver I] Title: 양, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3184. 양/양.py
+++ 백준/Silver/3184. 양/양.py
@@ -1,4 +1,3 @@
-from collections import deque as dq
 r,c=map(int,input().split())
 l=[list(input().strip()) for _ in range(r)]
 o,v=0,0
@@ -6,9 +5,9 @@ for i in range(r):
     for j in range(c):
         if(l[i][j]=='#'): continue
         s,w=0,0
-        q=dq([(i,j)])        
+        q=[(i,j)]
         while q:
-            x,y=q.popleft()            
+            x,y=q.pop(0)
             for n,m in ((0,0),(1,0),(-1,0),(0,1),(0,-1)):
                 if(0<=x+n<r and 0<=y+m<c and l[x+n][y+m]!='#'):
                     q.append((x+n,y+m))
