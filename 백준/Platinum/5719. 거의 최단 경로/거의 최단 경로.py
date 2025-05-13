ba51a4b3f1f237b30fe3f0e6077b9ebb0c6fc108
Seong-Min-Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-05-13 14:00:21 +0900

    [Platinum V] Title: 거의 최단 경로, Time: 308 ms, Memory: 120144 KB -BaekjoonHub

--- 백준/Platinum/5719. 거의 최단 경로/거의 최단 경로.py
+++ 백준/Platinum/5719. 거의 최단 경로/거의 최단 경로.py
@@ -2,15 +2,7 @@ import heapq
 from collections import deque as dq
 import sys
 I=sys.stdin.readline
-while True:
-    n,m=map(int,I().split())
-    if(n==m==0): break
-    s,d=map(int,I().split())
-    l={}
-    for _ in range(m):
-        u,v,p=map(int,I().split())
-        if(u in l): l[u].append((v,p))
-        else: l[u]=[(v,p)]
+def f(s,n,t):
     v=[1e9 for _ in range(n)]
     r=[[] for _ in range(n)]
     q=[(0,s)]
@@ -20,12 +12,15 @@ while True:
         if(v[p]<c): continue
         if(p not in l): continue
         for x,y in l[p]:
+            if(p in t and x in t[p]): continue
             if(v[x]>c+y):
                 v[x]=c+y
                 r[x]=[p]
                 heapq.heappush(q,(c+y,x))
             elif(v[x]==c+y):
                 r[x].append(p)
+    return v,r
+def g(d,n,r):
     t={}
     q=dq([d])
     v=[0 for _ in range(n)]
@@ -37,17 +32,18 @@ while True:
                 if(e in t): t[e].append(p)
                 else: t[e]=[p]
                 q.append(e)
-    v=[1e9 for _ in range(n)]
-    q=[(0,s)]
-    v[s]=0
-    while q:
-        c,p=heapq.heappop(q)
-        if(v[p]<c): continue
-        if(p not in l): continue
-        for x,y in l[p]:
-            if(p in t and x in t[p]): continue
-            if(v[x]>c+y):
-                v[x]=c+y
-                heapq.heappush(q,(c+y,x))
+    return t
+while True:
+    n,m=map(int,I().split())
+    if(n==m==0): break
+    s,d=map(int,I().split())
+    l={}
+    for _ in range(m):
+        u,v,p=map(int,I().split())
+        if(u in l): l[u].append((v,p))
+        else: l[u]=[(v,p)]
+    v,r=f(s,n,{})
+    t=g(d,n,r)
+    v,r=f(s,n,t)
     if(v[d]==1e9): print(-1)
     else: print(v[d])
