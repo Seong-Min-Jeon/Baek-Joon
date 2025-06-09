b3cf873d7ddcfe7b9cd9cb356b058e17603e4e7c
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-06-09 22:33:39 +0900

    [Gold V] Title: 엘 도라도, Time: 116 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Gold/6506. 엘 도라도/엘 도라도.py
+++ 백준/Gold/6506. 엘 도라도/엘 도라도.py
@@ -1,17 +1,14 @@
-import sys
-I=sys.stdin.readline
 while True:
-    n,k=map(int,I().split())
-    if(n==0): break
-    l=list(map(int,I().split()))
-    r=[[0 for _ in range(n)] for _ in range(k)]
-    for i in range(n):
-        r[0][i]=1
-    for i in range(1,k):
-        for j in range(n):
+    n,k=map(int,input().split())
+    if(n==k==0): break
+    d=[[0]*n for _ in range(k+1)]
+    l=list(map(int,input().split()))
+    for i in range(1,k+1):
+        for j in range(i-1,n):
+            if(i==1): d[i][j]=1; continue
             s=0
-            for m in range(j):
-                if(l[m]<l[j]):
-                    s+=r[i-1][m]
-            r[i][j]=s
-    print(sum(r[-1]))
+            for h in range(j):
+                 if(l[j]>l[h]):
+                    s+=d[i-1][h]
+            d[i][j]=s
+    print(sum(d[-1]))
