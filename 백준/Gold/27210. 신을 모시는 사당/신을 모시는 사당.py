6c51afe82ee73dccbe2b7ff65cb6959185193872
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-06-23 13:27:32 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 108 ms, Memory: 35288 KB -BaekjoonHub

--- 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.py
@@ -1,17 +1,9 @@
 n=int(input())
 t=list(map(int,input().split()))
-l,r=[],[]
+l,r=[1 if e==1 else -1 for e in t],[1 if e==2 else -1 for e in t]
+m,x,y=0,0,0
 for i in range(n):
-    if(t[i]==1): l.append(1); r.append(-1)
-    else: l.append(-1); r.append(1)
-m,s=0,0
-for e in l:
-    s+=e
-    if(s<0): s=0
-    m=max(m,s)
-s=0
-for e in r:
-    s+=e
-    if(s<0): s=0
-    m=max(m,s)
+    x=max(x+l[i],0)
+    y=max(y+r[i],0)
+    m=max(m,x,y)
 print(m)
