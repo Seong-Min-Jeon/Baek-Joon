49a1ec6ae8c760f0b653eaf63355ae1ec39b0cb5
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-06-26 14:26:07 +0900

    [Gold V] Title: A = B ⊕ C, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/33914. A ＝ B ⊕ C/A ＝ B ⊕ C.py
+++ 백준/Gold/33914. A ＝ B ⊕ C/A ＝ B ⊕ C.py
@@ -1,9 +1,7 @@
 import math
 x,y=map(int,input().split())
-M=1000000007
-n=(x+y)//3
-c=0
+c,n,M=0,(x+y)//3,1000000007
 while x!=2*y:
-    c+=1; y-=3
-    if(y<0): print(0); exit()
+ c+=1;y-=3
+ if(y<0):print(0);exit()
 print((pow(3,x//2,M)*math.comb(n,c))%M)
