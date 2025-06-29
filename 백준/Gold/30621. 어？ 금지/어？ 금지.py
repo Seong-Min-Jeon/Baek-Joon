1dc292f2cba24fada30ed7a6bebb213315a73739
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-06-29 15:40:04 +0900

    [Gold III] Title: 어? 금지, Time: 200 ms, Memory: 147628 KB -BaekjoonHub

--- 백준/Gold/30621. 어？ 금지/어？ 금지.py
+++ 백준/Gold/30621. 어？ 금지/어？ 금지.py
@@ -1,6 +1,5 @@
 import bisect
-import sys
-I=sys.stdin.readline
+I=input
 n=int(I())
 t=list(map(int,I().split()))
 b=list(map(int,I().split()))
@@ -8,7 +7,7 @@ c=list(map(int,I().split()))
 d=[0]*n
 d[0]=c[0]
 for i in range(1,n):
-    j=bisect.bisect_left(t,t[i]-b[i])-1
-    d[i]=max(d[i-1],c[i])
-    if(j>=0): d[i]=max(d[i],d[j]+c[i])
+ j=bisect.bisect_left(t,t[i]-b[i])-1
+ d[i]=max(d[i-1],c[i])
+ if(j>=0): d[i]=max(d[i],d[j]+c[i])
 print(d[-1])
