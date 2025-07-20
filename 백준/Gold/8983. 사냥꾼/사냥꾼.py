761e17bcf8177195c408ed600428866ae0a60873
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-07-20 14:19:05 +0900

    [Gold IV] Title: 사냥꾼, Time: 472 ms, Memory: 57932 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,13 +1,14 @@
-from collections import deque as d
+from bisect import bisect_left as l
 import sys
 I=sys.stdin.readline
-n,m,l=map(int,I().split())
+n,m,b=map(int,I().split())
 s=[*map(int,I().split())]
-s.sort()
-q=d([tuple(map(int,I().split())) for _ in range(m)])
-for i in range(n):
-    a=s[i]
-    for _ in range(len(q)):
-        x,y=q.popleft()
-        if(abs(a-x)+y>l): q.append((x,y))
-print(m-len(q))
+q=[[*map(int,I().split())] for _ in range(m)]
+s.sort(); q.sort()
+c=0
+for i in range(m):
+    x,y=q[i]
+    j=l(s,x)
+    if(j>0 and abs(s[j-1]-x)+y<=b): c+=1
+    elif(j<n and abs(s[j]-x)+y<=b): c+=1
+print(c)
