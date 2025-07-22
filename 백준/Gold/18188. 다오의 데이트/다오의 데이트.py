26a05185b53ef48726cad15089a839e8ce805a43
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-07-22 14:24:39 +0900

    [Gold IV] Title: 다오의 데이트, Time: 88 ms, Memory: 111252 KB -BaekjoonHub

--- 백준/Gold/18188. 다오의 데이트/다오의 데이트.py
+++ 백준/Gold/18188. 다오의 데이트/다오의 데이트.py
@@ -1,18 +1,21 @@
-def f(t,x,y):
- if(t in s and 0<=x<h and 0<=y<w and  l[x][y]!='@'):q.append((x,y,z+t))
-I=input;h,w=map(int,I().split())
+I=input
+h,w=map(int,I().split())
 l=[list(I().strip()) for _ in range(h)]
 n=int(I())
-c=[I().split() for _ in range(n)]
-s,g,q=0,0,0
+c=[set(I().strip().split()) for _ in range(n)]
+s,g=0,0
 for i in range(h):
- for j in range(w):
-  if(l[i][j]=='D'):s=(i,j,'');q=[s]
-  if(l[i][j]=='Z'):g=(i,j)
+    for j in range(w):
+        if(l[i][j]=='D'): s=(i,j,'')
+        if(l[i][j]=='Z'): g=(i,j)
+q=[s]
 while q:
- x,y,z=q.pop(0)
- if((x,y)==g): print(f'YES\n{z}'); exit()
- if(len(z)>=n): continue
- s=c[len(z)]
- f('W',x-1,y);f('S',x+1,y);f('A',x,y-1);f('D',x,y+1)
+    x,y,z=q.pop(0)
+    if((x,y)==g): print(f'YES\n{z}'); exit()
+    if(len(z)>=n): continue
+    s=c[len(z)]
+    if('W' in s and 0<=x-1<h and l[x-1][y]!='@'): q.append((x-1,y,z+'W'))
+    if('S' in s and 0<=x+1<h and l[x+1][y]!='@'): q.append((x+1,y,z+'S'))
+    if('A' in s and 0<=y-1<w and l[x][y-1]!='@'): q.append((x,y-1,z+'A'))
+    if('D' in s and 0<=y+1<w and l[x][y+1]!='@'): q.append((x,y+1,z+'D'))
 print('NO')
