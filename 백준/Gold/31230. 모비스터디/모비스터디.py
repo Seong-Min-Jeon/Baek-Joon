8e438506d476ea28af61629f108e56f6bcdc5a8b
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-07-23 13:57:58 +0900

    [Gold III] Title: 모비스터디, Time: 1196 ms, Memory: 214752 KB -BaekjoonHub

---
+++ 백준/Gold/31230. 모비스터디/모비스터디.py
@@ -0,0 +1,33 @@
+import sys
+import heapq
+I=sys.stdin.readline
+n,m,s,g=map(int,I().split())
+l=[[] for _ in range(n+1)]
+for _ in range(m):
+    a,b,c=map(int,I().split())
+    l[a].append((b,c))
+    l[b].append((a,c))
+r=[1e99]*(n+1)
+v=[[] for _ in range(n+1)]
+q=[(0,s)]
+r[s]=0
+while q:
+    d,p=heapq.heappop(q)
+    if(r[p]<d): continue
+    for i,e in l[p]:
+        if(d+e<r[i]):
+            r[i]=d+e
+            v[i].clear()
+            v[i].append(p)
+            heapq.heappush(q,(d+e,i))
+        elif(d+e==r[i]):
+            v[i].append(p)
+r=set()
+q=[g]
+while q:
+    a=q.pop()
+    r.add(a)
+    for e in v[a]:
+        q.append(e)
+print(len(r))
+print(*sorted(list(r)))
