9591171e565fff5082e72b62724ec7236bc1c770
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-08-12 10:38:41 +0900

    [Silver III] Title: 다음 순열, Time: 48 ms, Memory: 33564 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,10 @@
+n=int(input()); l=[*map(int,input().split())]
+if(sorted(l,reverse=True)==l): print(-1); exit()
+k,v=0,0
+for i in range(n):
+    if(l[i]>v): k=i
+    v=l[i]
+t=sorted(l[k-1:].copy())
+v=t[t.index(l[k-1])+1]
+t.remove(v)
+print(*l[:k-1],v,*t)
