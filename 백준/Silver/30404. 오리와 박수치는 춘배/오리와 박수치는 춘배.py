2d6bdbfe0bc524de8f29a792fc4842b7a664c13a
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-08-14 10:19:36 +0900

    [Silver IV] Title: 오리와 박수치는 춘배, Time: 72 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.py
+++ 백준/Silver/30404. 오리와 박수치는 춘배/오리와 박수치는 춘배.py
@@ -1,6 +1,5 @@
-n,k=map(int,input().split())
-l=[*map(int,input().split())]
+n,k,*l=list(map(int,open(0).read().split()))
 p,c=l[0]+k,1
-for i in range(n):
-    if(p<l[i]): p=l[i]+k; c+=1
+for e in l:
+ if(p<e):p=e+k;c+=1
 print(c)
