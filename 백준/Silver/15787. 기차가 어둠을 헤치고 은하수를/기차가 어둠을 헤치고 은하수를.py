154c122f92831227a6581d40477916ca33efff6c
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-08-17 14:59:14 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 136 ms, Memory: 34204 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -1,16 +1,12 @@
 import sys
 I=sys.stdin.readline
 n,m=map(int,I().split())
-l=[0b0]*(n+1)
+l=[0]*(n+1)
 for _ in range(m):
-    t=[*map(int,I().split())]
-    if(len(t)==3):
-        q,i,v=t
-        if(q==1): l[i]=l[i]|(1<<v)
-        else: l[i]=l[i]&~(1<<v)
-    else:
-        q,i=t
-        if(q==3): l[i]=l[i]&~(1<<20); l[i]=l[i]<<1
-        else: l[i]=l[i]&~(1<<1); l[i]=l[i]>>1
+    q,i,*v=map(int,I().split())
+    if(q==1): l[i]=l[i]|(1<<v[0])
+    elif(q==2): l[i]=l[i]&~(1<<v[0])
+    elif(q==3): l[i]=l[i]&~(1<<20); l[i]=l[i]<<1
+    else: l[i]=l[i]&~(1<<1); l[i]=l[i]>>1
 del l[0]
 print(len(set(l)))
