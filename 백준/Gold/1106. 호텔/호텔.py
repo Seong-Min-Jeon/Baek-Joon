bec5405bf245074f62ee4bcf55eaf9b60f65e29c
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-09-12 17:02:54 +0900

    [Gold IV] Title: 호텔, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,8 +1,8 @@
 c,n=map(int,input().split())
 l=[[*map(int,input().split())] for _ in range(n)]
-r=[1e99]*(1000*c)
+r=[1e99]*(100+c)
 r[0]=0
-for i in range(1000*c):
+for i in range(100+c):
     for j in range(n):
         d,p=l[j]
         r[i]=min(r[i],r[i-p]+d)
