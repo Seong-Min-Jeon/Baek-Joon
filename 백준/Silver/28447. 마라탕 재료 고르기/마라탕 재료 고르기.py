773209ca9ba1c145ed6f0a27c7796be5f6a2934d
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-09-20 13:24:54 +0900

    [Silver II] Title: 마라탕 재료 고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/28447. 마라탕 재료 고르기/마라탕 재료 고르기.py
+++ 백준/Silver/28447. 마라탕 재료 고르기/마라탕 재료 고르기.py
@@ -1,19 +1,14 @@
-def f():
-    global m,c
+def f(m,r):
+    global c
     if(len(r)==k): c=max(c,m); return
     for i in range(n):
-        if(i not in r):
+        if(not r or r[-1]<i):
             t=0
             for j in r:
                 t+=l[i][j]
-            r.append(i)            
-            m+=t
-            f()
-            m-=t
-            r.remove(i)
+            f(m+t,r+[i])
 n,k=map(int,input().split())
 l=[[*map(int,input().split())] for _ in range(n)]
-r=[]
-m,c=0,-1e9
-f()
+c=-1e9
+f(0,[])
 print(c)
