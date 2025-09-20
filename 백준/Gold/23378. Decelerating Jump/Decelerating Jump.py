1b9771d6684fd45cadda5c9202ea5a64e49284d5
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-09-20 15:33:15 +0900

    [Gold III] Title: Decelerating Jump, Time: 1436 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/23378. Decelerating Jump/Decelerating Jump.py
+++ 백준/Gold/23378. Decelerating Jump/Decelerating Jump.py
@@ -1,12 +1,8 @@
 n=int(input())
 l=[*map(int,input().split())]
-r=[[-1e99]*n for _ in range(n)]
-r[0]=[l[0]]*n
-for i in range(n):
-    t=[-1e99]*n
-    t[-1]=r[i][-1]
-    for k in range(n-2,-1,-1):
-        t[k]=max(t[k+1],r[i][k])
-    for j in range(1,n-i):
-        r[i+j][j]=max(r[i+j][j],t[j]+l[i+j])
-print(max(r[-1]))
+r=[-1e99]*n
+r[-1]=l[-1]
+for k in range(1,n):
+    for i in range(n-k-1,-1,-1):
+        r[i]=max(r[i],r[i+k]+l[i])
+print(r[0])
