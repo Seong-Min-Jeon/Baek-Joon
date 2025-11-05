dc5600ff1337e2d384251a5372f0f9f57d786976
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2025-11-05 17:27:40 +0900

    [Silver III] Title: blobnom.xyz, Time: 468 ms, Memory: 186932 KB -BaekjoonHub

--- 백준/Silver/33524. blobnom．xyz/blobnom．xyz.py
+++ 백준/Silver/33524. blobnom．xyz/blobnom．xyz.py
@@ -1,6 +1,4 @@
 from bisect import bisect_right as B
-I=input
-F=lambda:map(int,I().split())
-n,m=F()
+I,F=input,lambda:map(int,I().split());n,m=F()
 a,b,l=sorted([*F()]),[*F()],[3*i*(i-1)+1 for i in range(1,400)]
-for e in b: print(B(l,B(a,e)),end=' ')
+for e in b:print(B(l,B(a,e)),end=' ')
