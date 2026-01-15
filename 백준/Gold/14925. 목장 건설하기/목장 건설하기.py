a5db3902fc7987865674bcff97cf8a5e60ee990d
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-01-15 14:51:27 +0900

    [Gold IV] Title: 목장 건설하기, Time: 196 ms, Memory: 123024 KB -BaekjoonHub

---
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.py
@@ -0,0 +1,12 @@
+import sys
+I=sys.stdin.readline
+F=lambda:map(int,I().split())
+n,m=F()
+l=[list(F()) for _ in range(n)]
+r=[[0]*m for _ in range(n)]
+for i in range(n):
+    for j in range(m):
+        if(l[i][j]==0):
+            if(i==0 or j==0): r[i][j]=1
+            else: r[i][j]=min(r[i-1][j],r[i][j-1],r[i-1][j-1])+1
+print(max([max(r[i]) for i in range(n)]))
