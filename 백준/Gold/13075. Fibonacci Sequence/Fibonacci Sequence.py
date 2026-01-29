a493d024f64ce60ca0d8fe7d8157259dd4d2cf98
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-01-29 16:32:44 +0900

    [Gold II] Title: Fibonacci Sequence, Time: 120 ms, Memory: 111472 KB -BaekjoonHub

---
+++ 백준/Gold/13075. Fibonacci Sequence/Fibonacci Sequence.py
@@ -0,0 +1,20 @@
+def f(m,s):
+    if(s==1): return m
+    p=f(m,s//2)
+    q=g(p,p)
+    if(s%2==1): return g(q,m)
+    else: return q
+def g(a,b):
+    r=[[0]*2 for _ in range(2)]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                r[i][j]=(r[i][j]+a[i][k]*b[k][j])%M
+    return r
+import sys
+I=sys.stdin.readline
+M=10**9
+for _ in range(int(I())):
+    n=int(I())
+    m=[[1,1],[1,0]]
+    print(f(m,n)[0][1])
