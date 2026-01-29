cf5a9185d6949d8b4461111d818dcd2c16d34e32
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-01-29 16:31:05 +0900

    [Gold II] Title: 피보나치 수 6, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,17 +1,17 @@
-def m(x,y):
-    global M
-    t=[0]*4
-    t[0]=((x[0]*y[0])%M+(x[1]*y[2])%M)%M
-    t[1]=((x[0]*y[1])%M+(x[1]*y[3])%M)%M
-    t[2]=((x[2]*y[0])%M+(x[3]*y[2])%M)%M
-    t[3]=((x[2]*y[1])%M+(x[3]*y[3])%M)%M
-    return t
-
-def f(n):
-    if(n<=1): return [1,1,1,0]
-    t=f(n//2)
-    if(n%2==0): return m(t,t)
-    else: return m(m(t,t),[1,1,1,0])
-
-M=1000000007
-print(f(int(input()))[1])
+def f(m,s):
+    if(s==1): return m
+    p=f(m,s//2)
+    q=g(p,p)
+    if(s%2==1): return g(q,m)
+    else: return q
+def g(a,b):
+    r=[[0]*2 for _ in range(2)]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                r[i][j]=(r[i][j]+a[i][k]*b[k][j])%M
+    return r
+M=10**9+7
+n=int(input())
+m=[[1,1],[1,0]]
+print(f(m,n)[0][1])
