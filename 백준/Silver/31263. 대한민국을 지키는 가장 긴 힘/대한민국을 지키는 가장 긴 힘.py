85fd0b27156c94d2a6d5d526bbc6ba0eac5c76a7
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-02-14 21:20:28 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 88 ms, Memory: 109544 KB, Score: 64 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
@@ -5,8 +5,8 @@ c,p=0,0
 while True:
     if(i==n):break
     if(s[i]==0 and p//100!=0):
-        if(p//10==0): i-=3; c+=1; p=0; continue
-        else: i-=2; c+=1; p=0; continue
+        if(p%10==0): i-=2; c+=1; p=0; continue
+        else: i-=1; c+=1; p=0; continue
     if(p*10+s[i]<=641): p=p*10+s[i]
     else: c+=1; p=s[i]
     i+=1
