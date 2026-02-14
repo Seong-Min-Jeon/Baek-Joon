b63e323bf3944b653bcfab3aa5bcd6ec9fc9ba10
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-02-14 19:29:59 +0900

    [Bronze III] Title: Secret Location, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5340. Secret Location/Secret Location.py
@@ -0,0 +1,4 @@
+a,b,c=len(input().strip()),len(input().strip()),len(input().strip())
+x,y,z=len(input().strip()),len(input().strip()),len(input().strip())
+print(f'Latitude {a}:{b}:{c}')
+print(f'Longitude {x}:{y}:{z}')
