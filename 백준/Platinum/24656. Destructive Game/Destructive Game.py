86a37e1fd0230e7c7a760414cf45173fc6eec920
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-02-23 13:43:30 +0900

    [Platinum III] Title: Destructive Game, Time: 132 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/24656. Destructive Game/Destructive Game.py
+++ 백준/Platinum/24656. Destructive Game/Destructive Game.py
@@ -1,3 +1,36 @@
+# import sys
+# I=sys.stdin.readline
+# n=int(I())
+# x=0
+# for _ in range(n):
+#     a,b=map(int,I().split())
+#     l=[0]
+#     for m in range(1,a+1):
+#         g=set()
+#         for k in range(10**6):
+#             if(m-b**k<0): break
+#             g.add(l[m-b**k])
+#         for i in range(10**6):
+#             if(i not in g):
+#                 l.append(i)
+#                 break
+#     print(l)
+#     x^=l[-1]
+# print('Alice' if x else 'Bob')
+
+'''
+10 3
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+7 4
+[0, 1, 0, 1, 2, 0, 1, 0]
+
+15 4
+[0, 1, 0, 1, 2, 0, 1, 0, 1, 2, 0, 1, 0, 1, 2, 0]
+
+홀수면 0101 반복
+짝수면 0 1 0 1 ... 자기(2) 가 반복
+'''
+
 import sys
 I=sys.stdin.readline
 x=0
