933ba37cf0e936ef2fb5450ccfa39c3fe060bc60
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-03-11 15:07:48 +0900

    [Platinum I] Title: 이동, Time: 276 ms, Memory: 132316 KB -BaekjoonHub

---
+++ 백준/Platinum/1067. 이동/이동.py
@@ -0,0 +1,42 @@
+P=998244353
+G=3
+
+def ntt(a,inv=0):
+    n=len(a); j=0
+    for i in range(1,n):
+        b=n>>1
+        while j&b: j^=b; b>>=1
+        j^=b
+        if i<j: a[i],a[j]=a[j],a[i]
+    l=2
+    while l<=n:
+        w=pow(G,(P-1)//l,P)
+        if(inv): w=pow(w,P-2,P)
+        for i in range(0,n,l):
+            x=1
+            for j in range(i,i+l//2):
+                u,v=a[j],a[j+l//2]*x%P
+                a[j]=(u+v)%P
+                a[j+l//2]=(u-v)%P
+                x=x*w%P
+        l*=2
+    if(inv):
+        x=pow(n,P-2,P)
+        for i in range(n): a[i]=a[i]*x%P
+
+# 두 수의 convolution
+# a,b는 int 배열 [1, 2, 3]
+def conv(a,b):
+    n=1
+    while n<len(a)+len(b)-1: n<<=1
+    a=a+[0]*(n-len(a)); b=b+[0]*(n-len(b))
+    ntt(a); ntt(b)
+    c=[a[i]*b[i]%P for i in range(n)]
+    ntt(c,1)
+    return c[:len(a)+len(b)-n-1]
+
+n=int(input())
+a=[*map(int,input().split())]
+b=[*map(int,input().split())]
+c=conv(a+a,b[::-1])
+print(max(c[n-1:2*n]))
