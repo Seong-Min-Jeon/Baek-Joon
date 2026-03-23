8e0d42d5f7bb285621630a715366ae14547d2cd0
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-03-23 14:21:41 +0900

    [Gold I] Title: 고속도로, Time: 3300 ms, Memory: 60296 KB -BaekjoonHub

--- 백준/Gold/1884. 고속도로/고속도로.py
+++ 백준/Gold/1884. 고속도로/고속도로.py
@@ -1,21 +1,20 @@
-import sys, heapq
+import sys,heapq
 I=sys.stdin.readline
-k,n,r=int(I()),int(I()),int(I())
-l=[[] for _ in range(n+1)]
-for _ in range(r):
-    a,b,c,d=map(int,I().split())
-    l[a].append((b,c,d))
-r=[[1e9]*(k+1) for _ in range(n+1)]
+F=lambda:map(int,I().split())
+K,N,R=int(I()),int(I()),int(I())
+l=[[] for _ in range(N+1)]
+for _ in range(R):
+    a,b,c,d=F()
+    l[a].append((c,d,b))
+r=[[1e99]*(K+1) for _ in range(N+1)]
 r[1][0]=0
 q=[(0,0,1)]
 while q:
-    x,y,p=heapq.heappop(q)
-    if(r[p][y]<x): continue
-    for i,e,f in l[p]:
-        if(y+f<=k and r[i][y+f]>x+e):
-            r[i][y+f]=x+e
-            heapq.heappush(q,(x+e,y+f,i))
-t=set(r[n])
-if(1e9 in t): t.remove(1e9)
-if(len(t)==0): print(-1)
-else: print(min(t))
+    d,t,p=heapq.heappop(q)
+    if(r[p][t]<d): continue
+    for a,b,c in l[p]:
+        if(b+t<=K and r[c][b+t]>d+a):
+            r[c][b+t]=d+a
+            heapq.heappush(q,(d+a,b+t,c))
+x=min(r[N])
+print(x if x!=1e99 else -1)
