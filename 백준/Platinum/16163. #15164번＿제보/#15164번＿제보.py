241038ff2a3d1b069cb04134444aec780d79b7d2
Seongmin Jeon <51979029+Seong-Min-Jeon@users.noreply.github.com>
2026-03-25 15:09:44 +0900

    [Platinum V] Title: #15164번_제보, Time: 2384 ms, Memory: 146828 KB -BaekjoonHub

---
+++ 백준/Platinum/16163. #15164번＿제보/#15164번＿제보.py
@@ -0,0 +1,27 @@
+def manacher(s):
+    n=len(s)
+    d1=[0]*n
+    l=r=-1
+    for i in range(n):
+        k=1 if i>r else min(d1[l+r-i],r-i+1)
+        while i-k>=0 and i+k<n and s[i-k]==s[i+k]: k+=1
+        d1[i]=k
+        k-=1
+        if i+k>r:
+            l=i-k
+            r=i+k
+    d2=[0]*n
+    l=r=-1
+    for i in range(n):
+        k=0 if i>r else min(d2[l+r-i+1],r-i+1)
+        while i-k-1>=0 and i+k<n and s[i-k-1]==s[i+k]: k+=1
+        d2[i]=k
+        k-=1
+        if i+k>r:
+            l=i-k-1
+            r=i+k
+    return d1,d2
+
+s=input().strip()
+d1,d2=manacher(s)
+print(sum(d1)+sum(d2))
